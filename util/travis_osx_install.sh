2ecf3c987b0907c19006c5d2c5d483a5d315b551
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2018-02-03 21:52:53 +0100

    fix travis

--- util/travis_osx_install.sh
+++ util/travis_osx_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 brew update
 brew install ccache openssl
-cd /usr/local/include
-ln -s ../opt/openssl/include/openssl
-cd ~
+#cd /usr/local/include
+#ln -s ../opt/openssl/include/openssl
+#cd ~
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
 
 git clone https://github.com/MacPython/terryfy.git ~/terryfy
 source ~/terryfy/travis_tools.sh
