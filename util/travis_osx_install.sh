5fc3b9ed260b30d8796ecb01acf2177543633981
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2018-02-03 20:13:46 +0100

    try fix travis macos

--- util/travis_osx_install.sh
+++ util/travis_osx_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 brew update
-brew install ccache openssl aescrypt-packetizer
+brew upgrade
+brew install ccache openssl
+cd /usr/local/include
+ln -s ../opt/openssl/include/openssl
 
 git clone https://github.com/MacPython/terryfy.git ~/terryfy
 source ~/terryfy/travis_tools.sh
